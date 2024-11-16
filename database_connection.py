d83e12733a85e7e005186295944c7fd1db7a3975
mk307 <115074350+mk307@users.noreply.github.com>
2024-11-16 23:50:18 +0500

    database_connection.py

--- database_connection.py
+++ database_connection.py
@@ -2,11 +2,11 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 # Load datasets
-who_data = pd.read_csv('E:\My Data\Skills Development\Programming\Python\Projects\One Health - Sustainability\Data\WHO_merged_data.csv')
-worldbank_data = pd.read_csv('E:\My Data\Skills Development\Programming\Python\Projects\One Health - Sustainability\Data\WorldBank_merged_data.csv')
+who_data = pd.read_csv('path.csv')
+worldbank_data = pd.read_csv('path.csv')
 
 # Creating a connection to the MySQL database
-engine = create_engine('mysql+mysqlconnector://who_wb_user:OneHealth_Sustainability@localhost/who_wb_databases')
+engine = create_engine('mysql+mysqlconnector://[user_name]:[password]@localhost/[database_name]')
 
 # Insert data into SQL tables
 who_data.to_sql('who_data', engine, if_exists='replace', index=False)
